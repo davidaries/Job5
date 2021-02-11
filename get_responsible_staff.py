510490b0aeb017d423db7e9bd52d972e3e64ab71
Dan Peterson <powder.steep@gmail.com>
2021-02-11 12:21:52 -0700

    One more little documenation update

--- get_responsible_staff.py
+++ get_responsible_staff.py
@@ -5,7 +5,7 @@ get_staffer receives from the caller the protocol and step
   and then get_staffer to identify and individual staff with that role
   and finally gets their device id and returns it.
 
-Something here on get_other_staffers
+get_other_staffers gets other staffer(s) who can be responsible for the step
 """
 
 import initial_load_data as ild
@@ -14,28 +14,28 @@ from icecream import ic
 
 def get_device_out(protocol, step):
     staff_type = get_staff_type(protocol, step)  # get the responsible staff_type
-    staffer = get_staffer(staff_type)  # get the responsible staffer based on staff_type
-    device_out = ild.staff_device[staffer]  # then get the device_out based on the staffer
+    staffer = get_staffer(staff_type)            # get the responsible staffer based on staff_type
+    device_out = ild.staff_device[staffer]       # then get the device_out based on the staffer
     return device_out
 
 
-def get_staff_type(protocol, step):  # get the staff_type who is responsible for the step in the protocol
+def get_staff_type(protocol, step):              # get the staff_type responsible for the step in the protocol
     staffer_type = ild.protostep_staff[protocol][step]
     return staffer_type
 
 
-def get_staffer(staff_type):  # get a staffer who can be responsible for the step in the protocol
+def get_staffer(staff_type):  # get a staffer of the staff_type who can be responsible for the step in the protocol
     for s in ild.staffers:    # currently gets the first staffer who matches on staff_type, much more to be done!
         if ild.staffers[s]['~23'] == staff_type:
             return s
 
 
-def get_other_staffers(staff_type, current_staffer, list):  # get other staffers who can be responsible for the step
+def get_other_staffers(staff_type, current_staffer, list):  # get other staffer(s) who can be responsible for the step
     staff_choices = []
     for s in ild.staffers:
         if ild.staffers[s]['~23'] == staff_type and s is not current_staffer:
-            if not list:
-                return s
-            else:
-                staff_choices.append(s)
-    return staff_choices
+            if not list:       # if list is false
+                return s       # returns the first staffer who meets the criteria
+            else:              # if list it true
+                staff_choices.append(s)   # appends all staffers who meet the criteria
+    return staff_choices                  # and returns the list
